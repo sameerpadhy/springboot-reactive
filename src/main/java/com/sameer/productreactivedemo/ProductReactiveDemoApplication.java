53affa64373addc427630e94a0a1a855e83b6e7a
Sameer Ranjan Padhy <sameer.padhy01@gmail.com>
2022-01-16 18:01:12 +0530

    initial commit

---
+++ src/main/java/com/sameer/productreactivedemo/ProductReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sameer.productreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReactiveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sameer/productreactivedemo/ProductReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.productreactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
